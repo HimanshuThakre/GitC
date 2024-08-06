395a44f788b948d51e2df293020f3ab03344b7c3
Ayushithakre12 <66862454+Ayushithakre12@users.noreply.github.com>
2024-08-07 00:00:06 +0530

    Fixed Files

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,6 @@
-#include<stdio.h>
-
-int sum(int a, int b);
-
-void printtable(int n);
-int main() {
-    int n ; 
-    printf("enter no");
-    scanf("%d", &n);
-     
+oid swap(int a, int b) {
+   int t = a;
+   a = b;
+   b = t;
+   printf("a = %d & b = %d\n", a, b );
+}
