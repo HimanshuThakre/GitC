b21d8b78cf30ed187c981c5968a85baa4caf10a7
HimanshuThakre <thakrehimanshu1721@gmail.com>
2025-03-13 20:47:45 +0530

    fi

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,3 +1,13 @@
-a = 1
-b=1
-print(a+b)
+class student :
+    
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print(" new name")
+        
+        
+s1 = student("karan",97)
+print(s1.name, s1.marks)
+
+s2 = student("arjun",47)
+print(s2.name, s2.marks)
